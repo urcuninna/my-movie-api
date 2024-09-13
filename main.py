ce0547da25fe88df8eebce275db472a4f8d76b80
ROOBIN <erika00yesid@gmail.com>
2024-09-12 20:53:28 -0500

    Sesión 2 fastAPI

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI #importar fastAPI
+from fastapi.responses import HTMLResponse
+from movies_list import movies_list
 
 app=FastAPI() #Crea un instancia de la clase fastAPI
 app.title= "Mi primera aplicación de peliculas y análisis de datos"
@@ -6,5 +8,15 @@ app.version = "0.0.1"
 
 @app.get('/', tags=['Home']) #Se define la ruta
 def message(): #Definimos una funcion de la ruta
-    return "Hello World"
+    return HTMLResponse('<h1> Hello World </h1>')
 
+@app.get('/movies', tags=['Movies'])
+def movies():
+    return movies_list
+
+@app.get('/movies/{_id}',tags=['Movies'])
+def get_movie(id:int):
+    for item in movies_list:
+        if item['id']==id:
+            return item
+    return []
---
+++ movies_list.py
@@ -0,0 +1,72 @@
+movies_list=[
+    {
+        "id": 1,
+        "title": "Your Name",
+        "overview": "resumen pendiente...",
+        "year": 2016,
+        "rating": 5.0
+    },
+    {
+        "id": 2,
+        "title": "Friday the 13th: part 2",
+        "overview": "resumen pendiente...",
+        "year": 1981,
+        "rating": 5.0
+    },
+    {
+        "id": 3,
+        "title": "Ghostland",
+        "overview": "resumen pendiente...",
+        "year": 2018,
+        "rating": 5.0
+    },
+    {
+        "id": 4,
+        "title": "A Nightmare on Elm Street",
+        "overview": "resumen pendiente...",
+        "year": 1984,
+        "rating": 5.0
+    },
+    {
+        "id": 5,
+        "title": "The Blair Witch Project",
+        "overview": "resumen pendiente...",
+        "year": 1999,
+        "rating": 5.0
+    },
+    {
+        "id": 6,
+        "title": "In the Tall Grass",
+        "overview": "resumen pendiente...",
+        "year": 2019,
+        "rating": 5.0
+    },
+    {
+        "id": 7,
+        "title": "The Silence of the Lambs",
+        "overview": "resumen pendiente...",
+        "year": 1991,
+        "rating": 5.0
+    },
+    {
+        "id": 8,
+        "title": "The Perfection",
+        "overview": "resumen pendiente...",
+        "year": 2018,
+        "rating": 5.0
+    },
+    {
+        "id": 9,
+        "title": "The Girl who Leapt Through Time",
+        "overview": "resumen pendiente...",
+        "year": 2006,
+        "rating": 5.0
+    },
+    {
+        "id": 10,
+        "title": "Silent Hill",
+        "overview": "resumen pendiente...",
+        "year": 2006,
+        "rating": 5.0
+    }
+]
