386d26f1d3438aa8230548010218098bf930f4c7
ROOBIN <erika00yesid@gmail.com>
2024-09-14 18:32:33 -0500

    Funcion POST

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI #importar fastAPI
+from fastapi import FastAPI, Body #importar fastAPI
 from fastapi.responses import HTMLResponse
 from movies_list import movies_list
 
@@ -20,3 +20,22 @@ def get_movie(id:int):
         if item['id']==id:
             return item
     return []
+
+@app.get('/movies/',tags=["Movies"])
+def get_movies_by_category (category: str, year:int):
+    return [item for item in movies_list if item ['category']==category]
+
+@app.post('/movies',tags=['Movies'])
+def create_movie(id: int=Body (), title:str=Body(), overview:str=Body(), year:int=Body(),rating:float=Body(), category:str=Body()):
+     
+    movies_list.append({
+         "id": id,
+        "title": title,
+        "overview": overview,
+        "year": year,
+        "rating": rating,
+        "category": category
+    })
+
+    return movies_list
+     
